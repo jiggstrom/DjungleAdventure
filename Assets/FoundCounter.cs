e67b36a940a778b8ecc4040726ec6f97aa794e58
Jens Iggström <jens.iggstrom@yourvoice.se>
2017-11-01 14:37:42 +0100

    Initial commit

---
+++ Assets/FoundCounter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FoundCounter : MonoBehaviour
+{
+    static int counter = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "RigidBodyFPSController")
+        {
+            var textref = GameObject.Find("CountText").GetComponent<Text>();
+            counter++;
+            textref.text = counter.ToString();
+            if (counter == 1)
+            {
+                textref.color = Color.red;
+                //var win = GameObject.FindGameObjectWithTag("Finish");
+                //win.SetActive(true);
+            }
+            Destroy(this.gameObject);
+
+        }
+    }
+}
